18075a1e84955713fd3c663512443416370f3d12
potdartapan <70244540+potdartapan@users.noreply.github.com>
2023-07-31 14:17:46 -0400

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip_address" {
-    value = azurerm_linux_virtual_machine.linux_vm.public_ip_address
-}
-
-output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
-}
-
